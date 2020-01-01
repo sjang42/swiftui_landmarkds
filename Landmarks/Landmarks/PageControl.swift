f4b19b4596cb2a855c884e45a2ba7cf7ffe5076a
sjang42 <jexjang92@gmail.com>
2020-01-01 23:04:16 +0900

    complete ch9

---
+++ Landmarks/Landmarks/PageControl.swift
@@ -0,0 +1,43 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Jex Jang on 2019/12/29.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updageCurrentPage(sender:)), for: .valueChanged)
+        
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updageCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
